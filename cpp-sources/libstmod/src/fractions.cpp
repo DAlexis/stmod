13111b9aa99012c1745e24bc1a49f3a6be59839f
Aleksey Bulatov <AlexeyABulatov@yandex.ru>
2019-11-30 20:41:01 +0300

    WIP on fractions

--- cpp-sources/libstmod/src/fractions.cpp
+++ cpp-sources/libstmod/src/fractions.cpp
@@ -1,5 +1,10 @@
 #include "stmod/fractions.hpp"
 
+FractionsStorage::FractionsStorage()
+{
+
+}
+
 void FractionsStorage::clear_subiteration()
 {
     m_current_value = m_previous_value;
@@ -72,4 +77,69 @@ void FractionsStorage::set_values(std::vector<double>::const_iterator& values)
             v[i] = *(values++);
         }
     }
+}
+
+void FractionsStorage::resize_interpolate(SolutionInterpolatorFunc interpolator)
+{
+    m_current_value = interpolator(m_current_value);
+    reinit_additional_arrays();
+}
+
+std::vector<dealii::Vector<double>>& FractionsStorage::current()
+{
+    return m_current_value;
+}
+
+std::vector<dealii::Vector<double>>& FractionsStorage::previous()
+{
+    return m_previous_value;
+}
+
+std::vector<dealii::Vector<double>>& FractionsStorage::delta()
+{
+    return m_delta;
+}
+
+std::vector<dealii::Vector<double>>& FractionsStorage::rhs()
+{
+    return m_rhs;
+}
+
+
+void FractionsStorage::create_arrays(size_t fractions_count, unsigned int dimension)
+{
+    m_previous_value.clear();
+    m_current_value.clear();
+    m_delta.clear();
+    m_rhs.clear();
+
+    for (size_t i=0; i < fractions_count; i++)
+    {
+        m_previous_value.push_back(dealii::Vector<double>(dimension));
+        m_current_value.push_back(dealii::Vector<double>(dimension));
+        m_delta.push_back(dealii::Vector<double>(dimension));
+        m_rhs.push_back(dealii::Vector<double>(dimension));
+    }
+}
+
+void FractionsStorage::reinit_additional_arrays()
+{
+    unsigned int new_dim = m_previous_value[0].size();
+    for (size_t i = 0; i < m_previous_value.size(); i++)
+    {
+        m_current_value[i].reinit(new_dim);
+        m_delta[i].reinit(new_dim);
+        m_rhs[i].reinit(new_dim);
+    }
+}
+
+
+FractionRHSBase::FractionRHSBase(FractionsStorage& storage, size_t fraction_index) :
+    m_storage(storage), m_fraction_index(fraction_index)
+{
+}
+
+size_t FractionRHSBase::fraction_index()
+{
+    return m_fraction_index;
 }
--- cpp-sources/libstmod/stmod/fractions.hpp
+++ cpp-sources/libstmod/stmod/fractions.hpp
@@ -4,10 +4,15 @@
 #include "dsiterpp/integration.hpp"
 
 #include <deal.II/lac/vector.h>
+#include <functional>
+
+using SolutionInterpolatorFunc = std::function< std::vector<dealii::Vector<double>> (const std::vector<dealii::Vector<double>>& orig_solution) >;
 
 class FractionsStorage : public dsiterpp::IVariable
 {
 public:
+    FractionsStorage();
+
     void clear_subiteration() override;
     void add_rhs_to_delta(double m) override;
     void make_sub_iteration(double dt) override;
@@ -16,13 +21,37 @@ public:
     void collect_deltas(std::vector<double>& deltas) const override;
     void set_values(std::vector<double>::const_iterator& values) override;
 
+    void create_arrays(size_t fractions_count, unsigned int dimension);
+
+    void resize_interpolate(SolutionInterpolatorFunc interpolator);
+
+    std::vector<dealii::Vector<double>>& current();
+    std::vector<dealii::Vector<double>>& previous();
+    std::vector<dealii::Vector<double>>& delta();
+    std::vector<dealii::Vector<double>>& rhs();
+
 private:
+    void reinit_additional_arrays();
+
     std::vector<dealii::Vector<double>> m_previous_value;
     std::vector<dealii::Vector<double>> m_current_value;
     std::vector<dealii::Vector<double>> m_delta;
     std::vector<dealii::Vector<double>> m_rhs;
 
     using dealii_vector_size_type = dealii::Vector<double>::size_type;
+};
+
+class FractionRHSBase : public dsiterpp::IRHS
+{
+public:
+    FractionRHSBase(FractionsStorage& storage, size_t fraction_index);
+
+    size_t fraction_index();
+
+private:
+
+    FractionsStorage& m_storage;
+    size_t m_fraction_index;
 };
 
 #endif // FRACTIONS_HPP_INCLUDED
